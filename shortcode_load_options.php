4da4a1106fe20f898d827be890de51da242adda2
dbsnurr <dbsnurr@gmail.com>
2014-12-17 05:06:34 +0100

    Fix

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -18,6 +18,30 @@ Class ShortcodeLoad_Options {
 		if ( ! current_user_can('update_plugins') )
 			return;
 
+		add_settings_section( 
+			array($this, 'shortcode_load_default'),
+			'Essentials Front Page Options',
+			array($this, 'shortcode_load_default_options_callback'),
+			array($this, 'shortcode_load_default_options)'
+		);
+		add_settings_field(
+			array($this, 'shortcode_load_default_text'),
+			'Featured Post',
+			array($this, 'shortcode_load_default_text_callback'),
+			array($this, 'shortcode_load_default_options'),
+			array($this, 'shortcode_load_default')
+		);
+
+		register_setting('shortcode_load_default_options', 'shortcode_load_default_options');
+
+	}
+
+	function shortcode_load_default_options_callback() {
+		echo '<p>Default Options:</p>'; 
+	}	
+
+	function shortcode_load_default_text_callback() {
+		echo '<p>New Script Options:</p>'; 
 	}
 
 	function shortcode_load_options_page(  ) { 
@@ -30,9 +54,9 @@ Class ShortcodeLoad_Options {
 		?>
 		<div class="wrap">
 			<h2 class="nav-tab-wrapper">
-				<a href="#" class="nav-tab">Default Options</a>
-				<a href="#" class="nav-tab">New Script</a>
-				<a href="#" class="nav-tab">New Style</a>
+				<a href="#tab_one" class="nav-tab">Default Options</a>
+				<a href="#tab_two" class="nav-tab">New Script</a>
+				<a href="#tab_three" class="nav-tab">New Style</a>
 			</h2>
 
 			<form action='options.php' method='post'>
@@ -40,8 +64,17 @@ Class ShortcodeLoad_Options {
 				<h2>Shortcode Load</h2>
 				
 				<?php
-				settings_fields( 'pluginPage' );
-				do_settings_sections( 'pluginPage' );
+
+				if($active_tab == 'tab_one') {
+					settings_fields( 'shortcode_load_default_options' );
+					do_settings_sections( 'shortcode_load_default_options' );
+				} elseif($active_tab == 'tab_two') {
+					settings_fields( 'shortcode_load_new_script_options' );
+					do_settings_sections( 'shortcode_load_new_script_options' );
+				} elseif($active_tab == 'tab_three') {
+					settings_fields( 'shortcode_load_new_style_options' );
+					do_settings_sections( 'shortcode_load_new_style_options' );
+				}
 				submit_button();
 				?>
 				
