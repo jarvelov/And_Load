5fc5b3c7d2dc37c06738b9f380385742352623b7
dbsnurr <dbsnurr@gmail.com>
2014-12-17 04:49:22 +0100

    Fix

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -21,19 +21,32 @@ Class ShortcodeLoad_Options {
 	}
 
 	function shortcode_load_options_page(  ) { 
+		if( isset( $_GET[ 'tab' ] ) ) {  
+			$active_tab = $_GET[ 'tab' ];  
+		} else {
+			$active_tab = 'tab_one';
+		}
 
 		?>
-		<form action='options.php' method='post'>
-			
-			<h2>Shortcode Load</h2>
-			
-			<?php
-			settings_fields( 'pluginPage' );
-			do_settings_sections( 'pluginPage' );
-			submit_button();
-			?>
-			
-		</form>
+		<div class="wrap">
+			<h2 class="nav-tab-wrapper">
+				<a href="#" class="nav-tab">Default Options</a>
+				<a href="#" class="nav-tab">New Script</a>
+				<a href="#" class="nav-tab">New Style</a>
+			</h2>
+
+			<form action='options.php' method='post'>
+				
+				<h2>Shortcode Load</h2>
+				
+				<?php
+				settings_fields( 'pluginPage' );
+				do_settings_sections( 'pluginPage' );
+				submit_button();
+				?>
+				
+			</form>
+		</div><!-- ./wrap -->
 		<?php
 
 	}
