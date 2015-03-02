cf498883047505398e3c5400056930fc9358b24b
dbsnurr <dbsnurr@gmail.com>
2015-03-02 03:52:48 +0100

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -693,8 +693,11 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 			$srcname = basename($file_src, $type);
 			$file_src_base = dirname($file_src) . '/';
 
-			if($revision_override != false AND $revision_override <= $revision) {
-				$revision = $revision_override;
+			if($revision_override != false) {
+				if($revision_override <= $revision) {
+					$revision = $revision_override;
+				}
+
 				$file_src = $file_src_base . $srcname . $revision . "." . $type;
 			}
 		}
