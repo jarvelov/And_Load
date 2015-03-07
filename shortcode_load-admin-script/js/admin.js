4d2767d7106cff66901b2dc1316b4259fdd06ec6
dbsnurr <dbsnurr@gmail.com>
2015-03-07 19:00:21 +0100

    Update

--- shortcode_load-admin-script/js/admin.js
+++ shortcode_load-admin-script/js/admin.js
@@ -4,13 +4,19 @@ jQuery(document).ready(function() {
 	jQuery('#edit_file_revisions_select').change(function() {
 		var revision = this.value;
 		var urlBase = window.location;
+		var newUrl;
 
 		//URL GET parameters
-	    var idParam = location.search.split('id=')[1];
-	    var revisionParam = location.search.split('id=')[1];
+		var idParam = location.search.split('id=')[1];
+		var revisionParam = location.search.split('revision=')[1];
 
-		var newUrl = urlBase + "&revision=" + revision;
+		if(typeof(revisionParam) == 'undefined') {
+			newUrl = urlBase + '&revision=' + revision;
+		} else {
+			newUrl = urlBase + '&revision=' + revision;
+		}
 
+		console.log(urlBase);
 		console.log(newUrl);
 
 		location.href = newUrl;
