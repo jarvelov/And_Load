8eb03e2a8baec395eb98981e4e760597f7ad786e
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-13 21:23:40 +0200

    Update

--- shortcode_load-admin-script/js/ace_edit.js
+++ shortcode_load-admin-script/js/ace_edit.js
@@ -1,10 +1,11 @@
-//Register a listen event on any changes made with editor
-editor.getSession().on('change', contentChanged)
-
 //Get the new data and save it to the temporary textarea
 function contentChanged() {
 	var content = editor.session.getValue();
 	var textarea = document.getElementById('edit_file_temporary_textarea');
 	textarea.value = content;
-	//console.log("Content updated");
+}
+
+jQuery(document).ready(function() {
+	//Register a listen event on any changes made with editor
+	editor.getSession().on('change', contentChanged);
 }
