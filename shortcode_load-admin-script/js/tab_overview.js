65b9a94a2e47e2c4785f72dcecd158eeef1b147f
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-14 19:48:35 +0200

    Style update

--- shortcode_load-admin-script/js/tab_overview.js
+++ shortcode_load-admin-script/js/tab_overview.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
-    //Check for change in revision dropdown and redirect to URL with &revision=value param
-    jQuery('#overview-table').DataTable();
+    //Initialize overview-table as a DataTable
+    jQuery('#overview-table').dataTable( {
+    	"pagingType": "full_numbers"
+    });
 });
