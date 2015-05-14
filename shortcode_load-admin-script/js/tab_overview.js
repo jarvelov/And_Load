894454075e4ece4da081bebec99c0c31c5738245
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-14 19:53:21 +0200

    Style update

--- shortcode_load-admin-script/js/tab_overview.js
+++ shortcode_load-admin-script/js/tab_overview.js
@@ -1,6 +1,4 @@
 jQuery(document).ready(function() {
     //Initialize overview-table as a DataTable
-    jQuery('#overview-table').dataTable( {
-    	"pagingType": "full_numbers"
-    });
+    jQuery('#overview-table').dataTable();
 });
