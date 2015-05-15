14666a5c54aecd683028bb0d78cc833861c805ad
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-15 15:03:32 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -808,8 +808,8 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
                     settings_fields( 'shortcode_load_edit_file_options' );
                     do_settings_sections( 'shortcode_load_edit_file_options' );
 
-                    $attributes = array('class' => 'btn btn-lg btn-success');
-                    submit_button('Save file', 'primary', '', '', $attributes);
+                    $attributes = array( 'class' => array('btn', 'btn-lg', 'btn-success') );
+                    submit_button('Save file', 'primary', 'submit-form', true, $attributes);
                 }
 
                 ?>
