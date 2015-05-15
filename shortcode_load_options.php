e4fc65fc538d37caebd990a630b8315e7a6f9fc8
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-15 15:49:43 +0200

    Style Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -502,7 +502,7 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
             $html .= '<h2>No scripts or styles created yet!</h2>';
             $html .= '<p>To begin click the button below or the <strong><a href="?page=shortcode_load&amp;tab=tab_edit">"Edit file"</a></strong> tab above.</p>';
             $html .= '<p>For more info and help check out the <strong><a href="?page=shortcode_load&amp;tab=tab_help">Help</a></strong> tab</p>';
-            $html .= '<a class="btn btn-lg btn-block btn-info" href="?page=shortcode_load&amp;tab=tab_edit">New file</a>';
+            $html .= '<a id="get-started-button" class="btn btn-lg btn-block btn-info" href="?page=shortcode_load&amp;tab=tab_edit">New file &rarr;</a>';
             $html .= '</div>';
         }
 
