b0808319e443645f7debde97f8217e4fa1b347d6
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-17 14:40:23 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -664,7 +664,7 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
         /*Ace editor default print margin column
             hide this section if print margin is disabled */
         $editor_default_print_margin_column = isset ( $options_default['editor_default_print_margin_column'] ) ? $options_default['editor_default_print_margin_column'] : $args['editor_default_print_margin_column'];
-        $show_editor_default_print_margin_column = ( $editor_default_print_margin_column ) ? 'inline-block' : 'none'; //show setting section if default tab override is set to true
+        $show_editor_default_print_margin_column = ( $editor_default_print_margin ) ? 'inline-block' : 'none'; //show setting section if default tab override is set to true
 
         $html .= '<div id="editor_default_print_margin_column_setting" class="default_options_sub_setting" style="display:' . $show_editor_default_print_margin_column. '">';
         $html .= '<label class="control-label" title="Print margin column width. Default: 80."><strong><small>Print margin column</small></strong></label>';
