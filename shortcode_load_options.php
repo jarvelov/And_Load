ef955daf8f55a84c62f4db880c95e42cdcadcf2d
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-17 14:32:47 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -638,9 +638,9 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
         //Ace editor default tab size
         $editor_default_tab_size = isset ( $options_default['editor_default_tab_size'] ) ? $options_default['editor_default_tab_size'] : $args['editor_default_tab_size'];
-        $show_editor_default_tab_size = ( $editor_default_tab_size_override ) ? 'show' : 'hide'; //show setting section if default tab override is set to true
+        $show_editor_default_tab_size = ( $editor_default_tab_size_override ) ? 'inline-block' : 'none'; //show setting section if default tab override is set to true
 
-        $html .= '<div id="editor_default_tab_size_setting" class="default_options_sub_setting ' . $show_editor_default_tab_size . '">';
+        $html .= '<div id="editor_default_tab_size_setting" class="default_options_sub_setting" style="display:' . $show_editor_default_tab_size . '">';
         $html .= '<label class="control-label" title="Override tab size for all file types."><strong><small>Default tab size</small></strong></label>';
         $html .= '<input type="number" id="editor_default_tab_size" name="shortcode_load_default_options[editor_default_tab_size]" class="form-control" value="' . $editor_default_tab_size . '" />';
         $html .= '</div>'; // ./editor_default_tab_size_setting
