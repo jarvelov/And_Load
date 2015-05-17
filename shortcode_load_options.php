efb0be01c2b14bd580aae778e5451be363a6e87d
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-17 20:37:15 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -717,18 +717,6 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
             $html = '<div id="edit_file_input_container">';
 
-            /* Select revision dropdown */
-
-            $html .= '<label class="control-label">Current revision:</label>';
-            $html .= '<select id="edit_file_revisions_select" class="form-control" name="edit_file_revisions_select">';
-            for ($i=$revision; $i >= 0; $i--) {
-                $selected = ($current_revision==$i) ? ' selected="selected"' : '';
-                $html .= '<option value='.$i.$selected.'>'.$i.'</option>';
-            }
-            $html .= "</select>";
-
-            $html .= '</div>'; // ./edit_file_input_container
-
             $html .= '<label class="control-label">File: <em>' . $name . '</em></label>';
 
             /* Shortcode displayed in an input field */
@@ -737,7 +725,21 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
             $shortcode_display = 'shortcode_load id=' . $id;
 
-            $html .='<input type="text" id="edit_file_shortcode_display" class="form-control" name="shortcode_load_edit_file_options[edit_file_shortcode_display]" readonly=readonly value="['.$shortcode_display.']"/>';            
+            $html .='<input type="text" id="edit_file_shortcode_display" class="form-control" name="shortcode_load_edit_file_options[edit_file_shortcode_display]" readonly=readonly value="['.$shortcode_display.']"/>';
+
+            /* Select revision dropdown */
+
+            $html .= '<label class="control-label">Current revision:</label>';
+            $html .= '<select id="edit_file_revisions_select" class="form-control" name="edit_file_revisions_select">';
+            for ($i=$revision; $i >= 0; $i--) {
+                $selected = ($current_revision==$i) ? ' selected="selected"' : '';
+                $html .= '<option value='.$i.$selected.'>'.$i.'</option>';
+            }
+            $html .= '</select>';
+
+            $html .= '<div id="clearfix"></div>';
+
+            $html .= '</div>'; // ./edit_file_input_container
 
             echo $html;
 
