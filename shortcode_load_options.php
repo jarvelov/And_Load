1bef9c45465c58f074e7cba8b2bab2c0e8450558
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-17 21:59:55 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -771,11 +771,11 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
             //File name input
             $html .= '<label class="control-label">File name</label>';
-            $html .= '<input type="text" id="new_file_name" class="form-control" name="shortcode_load_edit_file_options[new_file_name]" placeholder="Enter a file name" />';
+            $html .= '<input type="text" id="new_file_name" class="form-control edit_file_input" name="shortcode_load_edit_file_options[new_file_name]" placeholder="Enter a file name" />';
 
             //File type select
             $html .= '<label class="control-label">File type</label>';
-            $html .= '<select id="new_file_type" class="form-control" name="shortcode_load_edit_file_options[new_file_type]"><option selected=selected value="plain_text">File type</option><option value="javascript">JavaScript</option><option value="css">CSS</option></select>';
+            $html .= '<select id="new_file_type" class="form-control edit_file_select" name="shortcode_load_edit_file_options[new_file_type]"><option selected=selected value="plain_text">File type</option><option value="javascript">JavaScript</option><option value="css">CSS</option></select>';
 
             //File upload
             $html .= '<div id="edit_file_file_upload_container">';
