d099507c2d03999e266763b868b86fbb1b71ef77
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-17 22:05:52 +0200

    Set sorting of overview to created_timestamp descending

--- shortcode_load-admin-script/js/tab_overview.js
+++ shortcode_load-admin-script/js/tab_overview.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function() {
     //Initialize overview-table as a DataTable
-    jQuery('#overview_table').dataTable();
+    jQuery('#overview_table').dataTable({
+    	"order": [[ 5, "desc" ]]
+    });
 });
