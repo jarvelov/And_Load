4305a21873100f5e198306d20c07710ab878a3a7
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-18 15:15:20 +0200

    Update

--- shortcode_load_minify.php
+++ shortcode_load_minify.php
@@ -8,7 +8,7 @@ Class ShortcodeLoad_Minify extends ShortcodeLoad {
         if( class_exists( 'ShortcodeLoad' ) ) {
             if ( ! ( class_exists( 'Minify' ) ) ) {
                 try {
-                    require(ShortcodeLoad::SLDIR . '/lib/minify/src/Minify.php');
+                    require(__FILE__ . '/lib/minify/src/Minify.php');
                 } catch(Exception $e) {
                     //var_dump($e);
                     throw new Exception("Error in loading minify library files", 6);
