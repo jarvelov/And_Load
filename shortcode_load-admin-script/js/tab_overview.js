39aaabf5f1d80077e6edbd9c2a6edc0a232f502b
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-18 12:46:45 +0200

    Update

--- shortcode_load-admin-script/js/tab_overview.js
+++ shortcode_load-admin-script/js/tab_overview.js
@@ -5,5 +5,9 @@ jQuery(document).ready(function() {
         jQuery('#overview_table').dataTable({
             "order": [[ overviewSettings['order_column'], overviewSettings['order_type'] ]]
         });
+
+        //add sorting_[order_type] to thead first row cell [order_column]
+        var order_column_thead = jQuery('#overview_table > thead')[0].rows[0].cells[overviewSettings['order_column']]
+        jQuery(order_column_thead).addClass('sorting_' + overviewSettings['order_type'] );
     }
 });
