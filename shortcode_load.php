e96911b0a2f911d25c0639083b5b6dcb5ec69f8a
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-20 00:21:35 +0200

    Update

--- shortcode_load.php
+++ shortcode_load.php
@@ -201,6 +201,7 @@ License:
 
         if( file_exists( $file ) ) {
             if( $is_script ) {
+                var_dump($dependencies);
                 wp_register_script( $name, $url, array($dependencies) );
                 wp_enqueue_script( $name );
             } else {
