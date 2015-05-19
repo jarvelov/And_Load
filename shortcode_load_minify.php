569b7d98d098fd022964c77334a96714591babb4
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-19 21:43:41 +0200

    Adding Path Converter required by Minify library

--- shortcode_load_minify.php
+++ shortcode_load_minify.php
@@ -9,6 +9,7 @@ Class ShortcodeLoad_Minify extends ShortcodeLoad {
             if ( ! ( class_exists( 'Minify' ) ) ) {
                 try {
                     require( dirname(__FILE__) . '/lib/minify/src/Minify.php' );
+                    require( dirname(__FILE__) . '/lib/path-converter/src/Converter.php' );
                 } catch(Exception $e) {
                     //var_dump($e);
                     throw new Exception("Error in loading minify library files", 6);
