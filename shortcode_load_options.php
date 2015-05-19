f46783096730f081645fce746233c6c04eae58ec
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-19 15:00:42 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -755,11 +755,12 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
             /* Select revision dropdown */
 
+            var_dump($revision);
+
             $html .= '<label class="control-label">Current revision:</label>';
             $html .= '<select id="edit_file_revisions_select" class="form-control edit_file_select" name="edit_file_revisions_select">';
             for ($i = $revision; $i >= 0; $i--) {
                 $selected = ($current_revision == $i) ? ' selected="selected"' : '';
-                $revision_name = ( $i == $revision) ? 'Latest' : ($i == 0) ? 'Source' : $i;
                 
                 if($i == $revision) {
                     $revision_name = 'Latest';
