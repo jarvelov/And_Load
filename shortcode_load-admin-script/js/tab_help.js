4082e296adafcd6bb71d15df892f5ffc45af6a74
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-22 00:38:57 +0200

    Update

--- shortcode_load-admin-script/js/tab_help.js
+++ shortcode_load-admin-script/js/tab_help.js
@@ -1,3 +1,19 @@
-jQuery(document).ready(function() {
+function scrollErrorIntoView() {
+    params = window.location.search.split('&');
+    for (var i = params.length - 1; i >= 0; i--) {
+        if( params[i].indexOf('error_id') >= 0 ) {
+            error_id = params[i].split('=')[1];
+            error_position = jQuery('#' + error_id).offset();
+            error_position['top'] -= error_position['top'] / 100;
+            jQuery('html, body').animate({scrollTop: error_position.top}, "slow");
 
+            return true;
+        }
+    }
+
+    return false; //no scroll was done
+}
+
+jQuery(document).ready(function() {
+    scrollErrorIntoView();
 });
