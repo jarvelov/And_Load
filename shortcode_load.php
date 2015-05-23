678a87fc399c2b371419ceef8ebc2d783835ba87
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-23 21:18:24 +0200

    Update with relative enqueue paths

--- shortcode_load.php
+++ shortcode_load.php
@@ -214,11 +214,11 @@ License:
 
         if( file_exists( $local_file_path ) ) {
             $file_url = get_site_url() . $file_path;
-            var_dump($file_url);
+            var_dump($file_url, 1);
             $this->shortcode_load_register_and_enqueue($name, $file_url, $is_script, $dependencies);
         } elseif( file_exists( $file_path ) ) { //variable is not a path within the plugin directory but may be somewhere else on the server, such as the wp-uploads directory
             $file_url = get_site_url() . $file_path;
-            var_dump($file_url);
+            var_dump($file_url, 2);
             $this->shortcode_load_register_and_enqueue($name, $file_url, $is_script, $dependencies);
         } elseif(! (filter_var($file_path, FILTER_VALIDATE_URL) === false) ) {
             //$file_path is an URL
