12e0164440f2d68e238d3af9a3fe7a49f66ccfe9
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-23 18:49:55 +0200

    Update

--- shortcode_load_options.php
+++ shortcode_load_options.php
@@ -1264,6 +1264,9 @@ Class ShortcodeLoad_Options extends ShortcodeLoad {
 
         $id = ( $args['edit_file_current_id'] ) ? $args['edit_file_current_id'] : NULL;
         $request = ( isset( $args['submit']) ) ? 'save' : ( ( isset( $args['delete'] ) ) ? 'delete' : NULL );
+
+        var_dump($request);
+        break;
        
         $file_datas = array();
 
