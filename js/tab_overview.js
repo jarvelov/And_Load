92b3aad374fa54f3fc924098fb72458ee7bcb427
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-26 12:04:57 +0200

    Update

--- js/tab_overview.js
+++ js/tab_overview.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+/*jQuery(document).ready(function() {
     //Initialize overview-table as a DataTable
 
     if(typeof(overviewSettings) != 'undefined' ) {
@@ -11,3 +11,4 @@ jQuery(document).ready(function() {
         jQuery(order_column_thead).addClass('sorting_' + overviewSettings['order_type'] );
     }
 });
+*/
