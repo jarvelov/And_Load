c32f43ff2d2167780c6737ec650e2401a859c680
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-27 20:19:14 +0200

    Update

--- and_load_options.php
+++ and_load_options.php
@@ -1146,7 +1146,7 @@ Class AndLoad_Options extends AndLoad {
         $html .= '<div id="and_load_donation_container">';
 
         $html .= '<p>If you like this plugin then consider donating to support it\'s development. It would mean a lot!</p>';
-        $html .= '<a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=tobias%2ejarvelov%40live%2ese&lc=US&item_name=And%20Load&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" /></a>';
+        $html .= '<a target="_blank" href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=tobias%2ejarvelov%40live%2ese&lc=US&item_name=And%20Load%20Wordpress%20Plugin&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" /></a>';
         $html .= '</div>'; //end and_load_donation_container
 
         $html .= '<div id="and_load_help_getting_started">';
