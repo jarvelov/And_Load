c6c1db9fbc95507411983a55408ce5b32083e5a3
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-27 17:54:59 +0200

    Updating directory structure

---
+++ js/tab_overview.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+    //Initialize overview-table as a DataTable
+
+    if(typeof(overviewSettings) != 'undefined' ) {
+        jQuery('#overview_table').dataTable({
+            "order": [[ overviewSettings['order_column'], overviewSettings['order_type'] ]]
+        });
+
+        //add sorting_[order_type] to thead first row cell [order_column]
+        order_column_thead = jQuery('#overview_table > thead')[0].rows[0].cells[overviewSettings['order_column']]
+        jQuery(order_column_thead).addClass('sorting_' + overviewSettings['order_type'] );
+    }
+});
