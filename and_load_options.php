16cf5af14e7e9f60c4b238a82ce56cddfc160e83
Tobias Järvelöv <tobias@jarvelov.se>
2015-05-27 19:24:59 +0200

    Update

--- and_load_options.php
+++ and_load_options.php
@@ -1146,7 +1146,7 @@ Class AndLoad_Options extends AndLoad {
         $html .= '<div id="and_load_help_getting_started">';
 
         $html .= '<span>Hello there! Cool that you\'re using <strong>And Load!</strong>. If you need some more examples than the documentation here there\'s more to find on the And Load project\'s <span class="glyphicon glyphicon-new-window"></span><a href="https://github.com/jarvelov/And_Load/blob/master/README.md">GitHub</a> page.</span>';
-        $html .= '<span>If you need support with the plugin check out the plugin\'s <span class="glyphicon glyphicon-new-window"><a href="https://wordpress.org/plugins/and_load">support page</a>. If you think you have found a bug please file a ticket on the project\'s GitHub page and I\'ll look into it as soon as possible</span>';
+        $html .= '<span>If you need support with the plugin check out the plugin\'s <span class="glyphicon glyphicon-new-window"></span><a href="https://wordpress.org/plugins/and_load">support page</a>. If you think you have found a bug please file a ticket on the project\'s GitHub page and I\'ll look into it as soon as possible</span>';
 
         $html .= '<div id="and_load_donation_container">';
         $html .= '<span>If you like this plugin then consider donating to support it\'s development. It would mean a lot and keep me motivated to keep it updated with neat stuff!</span>';
